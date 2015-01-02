3a5741fd6a468b84f5f37f0a4e6076262cae21ad
Arnstein Johansen <arnstej@gmail.com>
2015-01-02 21:03:48 +0100

    Styling of detail view.

--- js/components/PartnerDetailView.jsx
+++ js/components/PartnerDetailView.jsx
@@ -5,9 +5,28 @@ module.exports =
     React.createClass({
 
         render: function () {
+            var partner = this.props.selectedPartner;
+            var mailTo = "mailto:" + partner.email;
             return (
                 <div>
-                {this.props.selectedPartner.toString()}
+                <h3>{partner.name}{" "}<i className="glyphicon glyphicon-star-empty"></i></h3>
+                <address>
+                <strong>Adresse</strong><br/>
+                  {partner.address}<br/>
+                  {partner.zipCode} {partner.city}<br/><br/>
+                <strong>Kontakt</strong><br/>
+                  <i className="glyphicon glyphicon-earphone"></i> {partner.phone}<br/>
+                  <i className="glyphicon glyphicon-envelope"></i> <a href={mailTo}>{partner.email}</a><br/>
+                  <i className="glyphicon glyphicon-globe"></i> <a href={partner.webSite}>{partner.webSite}</a><br/>
+                </address>
+                <h4>Kontaktpersoner</h4>
+                <ul className="list-unstyled">
+                    {
+                        partner.contacts.map(function(contact){
+                           return <li>{contact.firstName}{" "}{contact.lastName} - <small>{contact.position}</small></li>  
+                        })
+                    }
+                </ul>
                 </div>
             );
         }
--- js/model/partner.js
+++ js/model/partner.js
@@ -34,5 +34,3 @@ Partner.prototype = {
 }
 
 module.exports = Partner;
-
-
