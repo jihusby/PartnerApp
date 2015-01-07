78f32488eda0a3182166b6e12a8c365d74c6c893
Torleif Sandnes <torleif.sandnes@itema.no>
2015-01-07 12:57:15 +0100

    Rewrote menu to not use react-bootstrap navbar. Need flexibility

--- js/components/PartnerApp.jsx
+++ js/components/PartnerApp.jsx
@@ -1,12 +1,6 @@
 var React = require("react");
 var Reflux = require("reflux");
 
-var Navbar = require("react-bootstrap/Navbar");
-var Nav = require("react-bootstrap/Nav");
-var NavItem = require("react-bootstrap/NavItem");
-
-var Button = require("react-bootstrap/Button");
-
 var MenuActions = require("../actions/MenuActions");
 var AuthActions = require("../actions/AuthActions");
 var MenuStore = require("../stores/MenuStore");
@@ -24,12 +18,11 @@ module.exports =
         mixins: [Reflux.connect(MenuStore,"menuItem"),Reflux.connect(AuthStore,"loginResult")],
 
         handleMenuToggle: function() {
-            console.log('hellow orld');
             this.setState({showMenu:!this.state.showMenu});
         },
 
         handleMenuSelect: function(menuEvent) {
-            this.setState({showMenu: false});
+            $('#nav-menu').collapse('hide');
             switch(menuEvent){
                 case Constants.MenuItems.home:
                     MenuActions.search();
@@ -47,6 +40,8 @@ module.exports =
                         AuthActions.logOut();      
                     }
                     break;
+                 default:
+                    console.error("Invalid menuItem");
             }
         },
 
@@ -66,28 +61,34 @@ module.exports =
                 case Constants.MenuItems.favourites:
                     content = <div>Favourites clicked</div>
                     break;
+
                 case Constants.MenuItems.login:
                     content = <Login />
                     break;
             }
-             if (this.state.showMenu) {
-             
-                var menu = (
-                    <Nav activeKey={this.state.menuItem} collapsable={true} expanded={false} onSelect={this.handleMenuSelect}>
-                        <NavItem eventKey={Constants.MenuItems.home}>Søk</NavItem>
-                        <NavItem eventKey={Constants.MenuItems.partnerlist}>Partnerliste</NavItem>
-                        <NavItem eventKey={Constants.MenuItems.favourites}>Favoritter</NavItem>
-                        <NavItem eventKey={Constants.MenuItems.login}>{ loginText }</NavItem>
-                    </Nav>
-                    );
-            } else {
-                menu = undefined;
-            }
-
             var navbar = (
-                <Navbar brand="ParnerApp" bsStyle="pills" toggleNavKey={1} inverse={true} navExpanded={false} onToggle={this.handleMenuToggle}>
-                    {menu}
-                </Navbar>
+                <nav className="navbar navbar-inverse navbar-fixed-top">
+                    <div className="container-fluid">
+                        <div className="navbar-header"> 
+                            <a className="navbar-brand">PartnerApp</a>
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div className="collapse navbar-collapse" id="nav-menu">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#" onClick={this.handleMenuSelect.bind(this, Constants.MenuItems.home)}>Søk</a></li>
+                                <li><a href="#" onClick={this.handleMenuSelect.bind(this, Constants.MenuItems.partnerlist)}>Partnerliste</a></li>
+                                <li><a href="#" onClick={this.handleMenuSelect.bind(this, Constants.MenuItems.favourites)}>Favoritter</a></li>
+                                <li><a href="#" onClick={this.handleMenuSelect.bind(this, Constants.MenuItems.login)}>{loginText}</a></li>
+                                <li><a href="#" onClick={this.handleMenuSelect.bind(this, Constants.MenuItems.home)}>Logg ut</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             );
 
             return (
