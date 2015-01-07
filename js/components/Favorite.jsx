a46aa34249f9da04493dc292ba0c92f95a64966f
goldnarms <arnstej@gmail.com>
2015-01-07 15:28:12 +0100

    Refactorings of favorites.

--- js/components/Favorite.jsx
+++ js/components/Favorite.jsx
@@ -6,10 +6,7 @@ var Constants = require("../utils/partner-constants");
 var FavoriteActions = require("../actions/FavoriteActions.js");
 var FavoriteStore = require("../stores/FavoriteStore.js");
 
-var Modal = require("react-bootstrap/Modal");
-var Button = require("react-bootstrap/Button");
 var OverlayMixin = require("react-bootstrap/OverlayMixin");
-var Input = require("react-bootstrap/Input");
 
 var FavoriteModal = require("./FavoriteModal.jsx");
 
@@ -29,7 +26,7 @@ module.exports = React.createClass({
         );
     } else{
         return  (
-            <a href="#" onClick={this.handleToggle}><i className="glyphicon glyphicon-star-empty"></i></a>
+            <a href="#" onClick={this.showModal}><i className="glyphicon glyphicon-star-empty"></i></a>
             );
         }
     },
@@ -37,7 +34,7 @@ module.exports = React.createClass({
   renderOverlay: function () {
         if(this.state.isModalOpen){
           return (
-                <FavoriteModal onToggle={this.handleToggle} addToFavorite={this.addToFavorite} />
+                <FavoriteModal onToggle={this.showModal} addToFavorites={this.addToFavorites} />
             );
         }else{
             return (
@@ -45,26 +42,8 @@ module.exports = React.createClass({
             );
         }
     },
-    
-    removeFavorite: function(){
-        var that = this;
-        var favorites = this.state.favorites || [];
-        
-        FavoriteActions.set(Constants.LocalStorageKeys.favorites, _.without(favorites,                                      _.find(favorites, function(f){ 
-                return f.id == that.props.id
-            })
-        ));
-        // force update
-        FavoriteActions.get(Constants.LocalStorageKeys.favorites);
-    },
-    
-    handleToggle: function () {
-        this.setState({
-          isModalOpen: !this.state.isModalOpen
-        });
-    },
 
-    addToFavorite: function(note){
+    addToFavorites: function(note){
         var favorites = this.state.favorites || [];
         var favorite = { id: this.props.id, note: note };
         FavoriteActions.set(Constants.LocalStorageKeys.favorites, _.union(favorites, [favorite]));
@@ -75,6 +54,24 @@ module.exports = React.createClass({
         });
     },
     
+    removeFavorite: function(){
+        var that = this;
+        var favorites = this.state.favorites || [];
+        
+        FavoriteActions.set(Constants.LocalStorageKeys.favorites, _.without(favorites, _.find(favorites, function(f){ 
+                return f.id == that.props.id
+            })
+        ));
+        // force update
+        FavoriteActions.get(Constants.LocalStorageKeys.favorites);
+    },
+    
+    showModal: function () {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+    },
+
     isFavorite: function(favorites){
         var that = this;
         return !!_.find(favorites, function(f){ return f.id == that.props.id});
--- js/components/FavoriteModal.jsx
+++ js/components/FavoriteModal.jsx
@@ -1,17 +1,14 @@
 var React = require("react");
-var Reflux = require("reflux");
-var _ = require("underscore");
 
 var Modal = require("react-bootstrap/Modal");
 var Button = require("react-bootstrap/Button");
 var Input = require("react-bootstrap/Input");
-var OverlayMixin = require("react-bootstrap/OverlayMixin");
-
-var FavoriteActions = require("../actions/FavoriteActions.js");
-var FavoriteStore = require("../stores/FavoriteStore.js");
 
 module.exports = React.createClass({
-    
+    propTypes: {
+        onToggle: React.PropTypes.func.isRequired,
+        addToFavorites: React.PropTypes.func.isRequired
+    },
     render: function(){
         return (
             <Modal title="Legg til i favoritter" onRequestHide={this.props.onToggle}>
@@ -20,14 +17,13 @@ module.exports = React.createClass({
               </div>
               <div className="modal-footer">
                 <Button onClick={this.props.onToggle}>Avbryt</Button>
-                <Button onClick={this.addToFavorite}>Legg til</Button>
+                <Button onClick={this.addToFavorites}>Legg til</Button>
               </div>
             </Modal>
           );
     },
     
-    addToFavorite: function(){
-        var func = this.props.addToFavorite;
-        func(this.refs.note.getValue());
+    addToFavorites: function(){
+        this.props.addToFavorites(this.refs.note.getValue());
     }
 });
