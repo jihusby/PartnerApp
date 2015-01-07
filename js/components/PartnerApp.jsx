f123aee585465538b17d278771e4cca7fbe6540d
Torleif Sandnes <torleif.sandnes@itema.no>
2015-01-07 09:17:53 +0100

    Fix brand in navbar

--- js/components/PartnerApp.jsx
+++ js/components/PartnerApp.jsx
@@ -83,9 +83,9 @@ module.exports =
             } else {
                 menu = undefined;
             }
-            var brand2 = (<h1>PartnerApp</h1>);
+
             var navbar = (
-                <Navbar brand=brand2 bsStyle="pills" toggleNavKey={1} inverse={true} navExpanded={false} onToggle={this.handleMenuToggle}>
+                <Navbar brand="ParnerApp" bsStyle="pills" toggleNavKey={1} inverse={true} navExpanded={false} onToggle={this.handleMenuToggle}>
                     {menu}
                 </Navbar>
             );
