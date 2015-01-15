f6f0e85ec6f6704d60223f5924e68bc6956ac5a4
goldnarms <arnstej@gmail.com>
2015-01-15 12:24:26 +0100

    Fixes issue #13. Invalid partner link.

--- js/components/PartnerDetailView.jsx
+++ js/components/PartnerDetailView.jsx
@@ -12,35 +12,103 @@ render: function () {
         return [contact.lastName, contact.firstName].join("_");
     });
 
-    var mailTo = "mailto:" + partner.email;
-    var phone = "tel:" + partner.phone;
+    var address = this.buildAddress(partner.address, partner.zipCode, partner.city);
+    var website = this.buildWebsite(partner.webSite);
+    var mailTo = this.buildMailTo(partner.email);
+    var phone = this.buildPhone(partner.phone);
+    
     var proffLink = "http://www.proff.no/bransjesøk?q=" + partner.name;
-    var mapLink = "http://maps.google.com/maps?q=" + encodeURIComponent(partner.address + ", " + partner.zipCode + ", " + partner.city + ", " + "norway");
+    var mapLink = this.buildMapLink(partner.address, partner.zipCode, partner.city);
+
     var i = 0;
+    
     return (
         <div>
             <h3>{partner.name}{" "} <small>{partner.partnerType}</small></h3>
             <address>
-            <strong>Adresse</strong><br/>
-                {partner.address}<br/>
-                {partner.zipCode} {partner.city}<br/><br/>
+            {address}
             <strong>Kontakt</strong><br/>
-                <i className="glyphicon glyphicon-earphone"></i> <a href={phone}>{partner.phone}</a><br/>
-                <i className="glyphicon glyphicon-envelope"></i> <a href={mailTo}>{partner.email}</a><br/>
-                <i className="glyphicon glyphicon-globe"></i> <a href={partner.webSite}>{partner.webSite}</a><br/>
+                {phone}
+                {mailTo}
                 <i className="glyphicon glyphicon-info-sign"></i> <a href={proffLink}>{partner.name} hos proff.no</a><br/>
-                <i className="glyphicon glyphicon-map-marker"></i> <a href={mapLink} target="_blank">Åpne i kart</a><br/>
+                {mapLink}                
             </address>
             <h4>Kontaktpersoner</h4>
             <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
                 {
                     sortedContacts.map(function(contact){                            
-                        i++;
-                        return <ContactDetailView id={contact.id} index={i} />
+                        return <ContactDetailView id={contact.id} index={i++} />
                     })
                 }
             </div>
         </div>
     );
+    },
+    
+    buildAddress: function(address, zipCode, city){
+        if(address && address.length > 0){
+        return (
+            <div>
+                <strong>Adresse</strong><br/>
+                {address}<br/>
+                {zipCode} {city}<br/><br/>
+            </div>
+                );
+        } else{
+            return ("");
+        }
+    },
+    
+    buildWebsite: function(website){
+        if(website && website.length > 0){
+            var hasHttp = website.indexOf("http://") == 0;
+            var websiteLink = hasHttp ? website : "http://" + website;
+            return (
+            <span>
+                <i className="glyphicon glyphicon-globe"></i> <a href={websiteLink}>{websiteLink}</a><br/>
+                </span>
+            );
+        } else {
+            return ("");
+        }
+    },
+    
+    buildMailTo: function(email){
+        if(email && email.length > 0){
+            var mailTo = "mailto:" + email;
+            return (
+                <span>
+                    <i className="glyphicon glyphicon-envelope"></i> <a href={mailTo}>{email}</a><br/>
+                </span>
+                );
+        } else {
+            return ("");
+        }
+    },
+    
+    buildPhone: function(phone){
+        if(phone && phone.length > 0){
+            var phoneLink = "tel:" + phone;
+            return (
+                <span>
+                    <i className="glyphicon glyphicon-earphone"></i> <a href={phoneLink}>{phone}</a><br/>
+                </span>
+            );
+        }else{
+            return ("");
+        }
+    },
+    
+    buildMapLink: function(address, zipCode, city){
+        if(address && address.length > 0){
+            var mapLink = "http://maps.google.com/maps?q=" + encodeURIComponent(address + ", " + zipCode + ", " + city + ", " + "norway");
+            return (
+            <span>
+                <i className="glyphicon glyphicon-map-marker"></i> <a href={mapLink}>Åpne i kart</a><br/>             </span>
+            );
+        } else {
+            return ("");
+        }
     }
+    
 });
