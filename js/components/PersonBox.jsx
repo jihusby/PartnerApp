9f1d17878d47d5666b6cf3b3779a9d4d36c0ad99
Arnstein Johansen <arnstej@gmail.com>
2015-01-19 09:48:35 +0100

    Fixes issue #22 Search layout

--- js/components/PersonBox.jsx
+++ js/components/PersonBox.jsx
@@ -8,7 +8,7 @@ module.exports = React.createClass({
         return (
             <a className="list-group-item" onClick={this.onClickPerson.bind(this, this.props.person.id)}>
                 <h4 className="list-group-item-heading">{this.props.person.firstName}{" "}{this.props.person.lastName}</h4>
-                <p className="list-group-item-text">{this.props.person.partnerName}</p>
+                <p className="list-group-item-text firm-list-item">{this.props.person.partnerName}</p>
             </a>
         );
     }
