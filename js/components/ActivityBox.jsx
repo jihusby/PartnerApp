1c19cebd4dd9f7edfd47fa1754b3ab9a16a8db63
Arnstein Johansen <arnstej@gmail.com>
2015-01-22 12:39:10 +0100

    Added dates to activities

--- js/components/ActivityBox.jsx
+++ js/components/ActivityBox.jsx
@@ -1,14 +1,32 @@
 var React = require("react");
+var moment = require("moment");
 
 module.exports = React.createClass({
     onClickActivity: function(id) {
         routie("activity/" + id);
     },
     render: function() {
+        var endDate ="";
+        var date = "";
+        var activity = this.props.activity;
+        var dateFormat = "DD.MM YYYY HH.mm";
+        if(activity.startDate){
+            date = moment(activity.startDate).format(dateFormat);
+            if(activity.endDate){
+                date = date + " - " + moment(activity.endDate).format(dateFormat);
+            }
+        }
         return (
-            <a className="list-group-item" onClick={this.onClickActivity.bind(this, this.props.activity.id)}>
-                <h4 className="list-group-item-heading">{this.props.activity.titleShort}</h4>
-                <p className="list-group-item-text firm-list-item">{this.props.activity.location}</p>
+            <a className="list-group-item" onClick={this.onClickActivity.bind(this, activity.id)}>
+                <h4 className="list-group-item-heading">{activity.titleShort}</h4>
+                <div className="row">
+                    <div className="col-xs-6">
+                        <p className="list-group-item-text">{date}</p>   
+                    </div>
+                    <div className="col-xs-6">
+                        <p className="list-group-item-text firm-list-item">{activity.location}</p>
+                    </div>
+                </div>
             </a>
         );
     }
