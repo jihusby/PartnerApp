029a459852344f5daa3d2cb5c827d910f3c1d71e
Arnstein Johansen <arnstej@gmail.com>
2015-01-22 12:39:27 +0100

    Added date and location to detailview for activity

--- js/components/ActivityDetailView.jsx
+++ js/components/ActivityDetailView.jsx
@@ -1,5 +1,6 @@
 var React = require("react");
 var _ = require("underscore");
+var moment = require("moment");
 var store = require("store.js");
 
 var Constants = require("../utils/partner-constants.js");
@@ -10,9 +11,20 @@ module.exports = React.createClass({
         var activities = store.get(Constants.LocalStorageKeys.activities);
         var activity = _.find(activities, function(a){return a.id == id;});
         var html = activity.description.replace(/["]/g, "");
+        var endDate ="";
+        var date = "";
+        var dateFormat = "DD.MM YYYY HH.mm";
+        if(activity.startDate){
+            date = moment(activity.startDate).format(dateFormat);
+            if(activity.endDate){
+                date = date + " - " + moment(activity.endDate).format(dateFormat);
+            }
+        }
         return (
             <div>
                 <h3>{activity.title}</h3>
+                <h4>{activity.location}</h4>
+                <p>{date}</p>
                 <span dangerouslySetInnerHTML={{__html:html}}></span>
             </div>
         );
