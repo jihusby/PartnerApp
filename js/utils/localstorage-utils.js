094ac9cc8c1aa592825d9e25d0f416ab061ec10f
Jan Inge Husby <jihusby@gmail.com>
2015-01-26 11:27:09 +0100

    Change localstorage-utils to reflect latest changes in datastore

--- js/utils/localstorage-utils.js
+++ js/utils/localstorage-utils.js
@@ -2,17 +2,19 @@ var React = require("react");
 var _ = require("underscore");
 var Constants = require("../utils/partner-constants");
 var store = require("store.js");
-var key = Constants.LocalStorageKeys.partnerdata;
-var data = store.get(key);
+var partnerkey = Constants.LocalStorageKeys.partnerdata;
+var contactkey = Constants.LocalStorageKeys.persons;
+var contactdata = store.get(contactkey);
+var partnerdata = store.get(partnerkey);
 
 module.exports = {
 
     findContact: function(id) {
-        return _.find(data.persons, function(person){ return person.id == id; });
+        return _.find(contactdata, function(person){ return person.id == id; });
     },
     
     findPartner: function(id) {
-        return _.find(data.partners, function(partner){ return partner.id == id; });
+        return _.find(partnerdata, function(partner){ return partner.id == id; });
     }
 }
 
