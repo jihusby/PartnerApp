6171ca81a294f1545c3fc0ea5897b4003cd39648
goldnarms <arnstej@gmail.com>
2015-02-05 15:23:29 +0100

    Showing enrollments for activities

--- js/components/PersonBox.jsx
+++ js/components/PersonBox.jsx
@@ -1,17 +1,25 @@
 var React = require("react");
 var Navigator = require("../utils/navigator");
+var _ = require("underscore");
 
 module.exports = React.createClass({
+    propTypes: {
+        freeText: React.PropTypes.string,
+        partnerId: React.PropTypes.number.isRequired,
+        partners: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+    },
+    
     mixins: [Navigator],
     
-    onClickPerson: function(id) {
-        this.goTo("person/" + id);
-    },
     render: function() {
+        var partnerId = this.props.partnerId;
+        var partner = _.find(this.props.partners, function(p){
+            return p.id === partnerId;
+        });
         return (
-            <a className="list-group-item" onClick={this.onClickPerson.bind(this, this.props.person.id)}>
-                <h4 className="list-group-item-heading">{this.props.person.firstName}{" "}{this.props.person.lastName}</h4>
-                <p className="list-group-item-text firm-list-item">{this.props.person.partnerName}</p>
+            <a className="list-group-item">
+                <h4 className="list-group-item-heading">{this.props.freeText}</h4>
+                <p className="list-group-item-text firm-list-item">{partner.name}</p>
             </a>
         );
     }
