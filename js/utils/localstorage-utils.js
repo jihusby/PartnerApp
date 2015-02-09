39f9909ee5dabac5d27986efb7127d50e0c2ae84
Jan Inge Husby <jihusby@gmail.com>
2015-02-09 08:47:02 +0100

    Fixed bug leading to marked favorites not showing in favorite list

--- js/utils/localstorage-utils.js
+++ js/utils/localstorage-utils.js
@@ -2,17 +2,21 @@ var React = require("react");
 var _ = require("underscore");
 var Constants = require("../utils/partner-constants");
 var store = require("store.js");
-var contactdata = store.get(Constants.LocalStorageKeys.persons);
-var partnerdata = store.get(Constants.LocalStorageKeys.partnerdata);
+var contactdata = '';
+var partnerdata = '';
 var ContactActions = require("../actions/ContactActions.js");
 
 module.exports = {
 
     findContact: function(id) {
-        return _.find(contactdata, function(person){ return person.id == id; });
+        contactdata = store.get(Constants.LocalStorageKeys.persons);
+        return _.find(contactdata, function(person){
+            return person.id == id;
+        });
     },
     
     findPartner: function(id) {
+        partnerdata = store.get(Constants.LocalStorageKeys.partnerdata);
         return _.find(partnerdata, function(partner){ return partner.id == id; });
     },
 
