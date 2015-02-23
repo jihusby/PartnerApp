68e6ef4760afcba0bd3113639036d53852673ddc
goldnarms <arnstej@gmail.com>
2015-02-23 15:41:43 +0100

    Revert "Addresses #99"
    This reverts commit a2b068b1ba8d287a284737ac9fe346c02d863a7d.
    
    Conflicts:
    	css/main.css

--- js/components/main.jsx
+++ js/components/main.jsx
@@ -21,7 +21,7 @@ module.exports =
                         <PartnerApp partners={this.state.rbkData.partners} contacts={this.state.rbkData.persons} activities={this.state.rbkData.activities} partnerTypes={this.state.rbkData.partnerTypes} isUpdating={this.state.rbkData.isUpdating} />
 
                     <div className="footer">
-                            Utviklet av <img src="images/itema-logo_graa_xxxsmall.png" className="footer-logo" />
+                            Utviklet av <img src="images/itema-logo_graa_xxsmall.png" className="footer-logo" />
                     </div>
                     </div>
                 </div>
