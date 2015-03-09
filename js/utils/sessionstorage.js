033ae24146e2208e53372489a955650d6318d753
goldnarms <arnstej@gmail.com>
2015-03-09 11:12:09 +0100

    #134, Possible solution for slow ios

--- js/utils/sessionstorage.js
+++ js/utils/sessionstorage.js
@@ -1,13 +1,13 @@
 module.exports = {
     set: function(key, value){
-        sessionStorage.setItem(key, value);   
+        window.sessionStorage.setItem(key, value);   
     },
     
     get: function(key){
-        return sessionStorage.getItem(key);
+        return window.sessionStorage.getItem(key);
     },
     
     remove: function(key){
-        sessionStorage.removeItem(key);
+        window.sessionStorage.removeItem(key);
     }
 }
